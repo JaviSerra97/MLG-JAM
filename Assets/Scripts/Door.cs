a9ec494ae1f5c735e3fe8d20f36b37af254ec7e7
jose manuel herrera vera <josem.hv@hotmail.es>
2021-01-29 19:39:04 +0100

    Door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Door : MonoBehaviour
+{
+    public float OpenTime, Closetime;
+    public float RotationToOpen, RotationToClose;
+
+    private bool TriggerStay;
+
+    private void Awake()
+    {
+        TriggerStay = false;
+    }
+
+    private void Update()
+    {
+        if(!TriggerStay )
+            CloseDoor();
+        else if(TriggerStay )
+            OpenDoor();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TriggerStay = true;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TriggerStay = false;
+        }
+    }
+    
+    private void OpenDoor()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.EulerAngles(new Vector3(0, RotationToOpen, 0)), OpenTime);
+    }
+
+    private void CloseDoor()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.EulerAngles(new Vector3(0, RotationToClose, 0)), Closetime);
+    }
+}
