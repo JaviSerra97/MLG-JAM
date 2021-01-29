87a0af06d414735553b307c901ca1a623f0e6d3c
jose manuel herrera vera <josem.hv@hotmail.es>
2021-01-29 21:52:04 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    public int hit;
+
+    public void execute()
+    {
+        switch (hit)
+        {
+            case 0:
+                Debug.Log("Cristal roto");
+                break;
+            case 1:
+                Debug.Log("Cuchillo");
+                break;
+            case 2:
+                Debug.Log("Osito de peluche");
+                break;
+        }
+    }
+}
