2fb74cb49e6c483f338fae25f71eb71771b0034a
Javier <jasercam97@gmail.com>
2021-01-29 18:13:18 +0100

    Selector idioma / Dialogos

---
+++ Assets/Scripts/LanguageSystem/LanguageSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSelector : MonoBehaviour
+{
+    public static LanguageSelector instance;
+
+    public bool english = true;
+
+    private void Awake()
+    {
+        if (!instance) { instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    public void SelectEnglish()
+    {
+        Debug.Log("English");
+    }
+
+    public void SelectSpanish()
+    {
+        Debug.Log("Spanish");
+    }
+}
