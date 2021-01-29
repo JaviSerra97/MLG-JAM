9ad357e675a0dbaffc6b7aeb233b1aef00efa1b8
Javier <jasercam97@gmail.com>
2021-01-29 19:12:57 +0100

    Selector idioma menus

--- Assets/Scripts/LanguageSystem/LanguageSelector.cs
+++ Assets/Scripts/LanguageSystem/LanguageSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LanguageSelector : MonoBehaviour
 {
@@ -8,19 +9,49 @@ public class LanguageSelector : MonoBehaviour
 
     public bool english = true;
 
+    public TMP_Dropdown languageDropdown;
+
+    public CanvasText[] listOfCanvasTexts;
+
     private void Awake()
     {
         if (!instance) { instance = this; }
         else { Destroy(gameObject); }
+
+        DontDestroyOnLoad(gameObject);
+
+        listOfCanvasTexts = GameObject.FindObjectsOfType<CanvasText>();
     }
 
-    public void SelectEnglish()
+    private void Update()
     {
-        Debug.Log("English");
+        if (Input.GetKeyDown(KeyCode.E)) { SelectEnglish(); }
+        if (Input.GetKeyDown(KeyCode.S)) { SelectSpanish(); }
     }
 
-    public void SelectSpanish()
+    public void SelectLanguage()
     {
-        Debug.Log("Spanish");
+        if(languageDropdown.value == 0) { SelectEnglish(); }
+        else if (languageDropdown.value == 1) { SelectSpanish(); }
+    }
+
+    void SelectEnglish()
+    {
+        english = true;
+
+        foreach(CanvasText CT in listOfCanvasTexts)
+        {
+            CT.SetEnglish();
+        }
+    }
+
+    void SelectSpanish()
+    {
+        english = false;
+
+        foreach(CanvasText CT in listOfCanvasTexts)
+        {
+            CT.SetSpanish();
+        }
     }
 }
