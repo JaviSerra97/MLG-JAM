49cec702e7b6dc6c0abf70a6c42dde5a6d33d67c
Javier <jasercam97@gmail.com>
2021-01-30 16:14:30 +0100

    Tween Menu

---
+++ Assets/Scripts/Tween/MenuTweens.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuTweens : MonoBehaviour
+{
+    public Transform mainPanel;
+    public Transform creditsPanel;
+    public Transform settingsPanel;
+
+    public float transitionDuration = .5f;
+
+    public void MainPanelTween()
+    {
+        transform.DOMove(mainPanel.position, transitionDuration)
+            .SetEase(Ease.InQuad)
+            .Play();
+    }
+
+    public void CreditsPanelTween()
+    {
+        transform.DOMove(creditsPanel.position, transitionDuration)
+            .SetEase(Ease.InQuad)
+            .Play();
+    }
+
+    public void SettingsPanelTween()
+    {
+        transform.DOMove(settingsPanel.position, transitionDuration)
+            .SetEase(Ease.InQuad)
+            .Play();
+    }
+}
