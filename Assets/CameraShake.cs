edef4db5824c65457c668196fddb21e09a3b3365
jose manuel herrera vera <josem.hv@hotmail.es>
2021-01-31 12:49:12 +0100

    CameraShake

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -3,16 +3,8 @@ using System.Collections;
 
 public class CameraShake : MonoBehaviour
 {
-	// Transform of the camera to shake. Grabs the gameObject's transform
-	// if null.
 	public Transform camTransform;
-
-	// How long the object should shake for.
-	public float shakeDuration = 0f;
-
-	// Amplitude of the shake. A larger value shakes the camera harder.
 	public float shakeAmount = 0.7f;
-	public float decreaseFactor = 1.0f;
 
 	Vector3 originalPos;
 
@@ -24,30 +16,30 @@ public class CameraShake : MonoBehaviour
 		}
 	}
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-		if (collision.gameObject.CompareTag("Player"))
-        {
+		if (other.gameObject.CompareTag("Player"))
+		{
 			originalPos = camTransform.localPosition;
 			Debug.Log("EnterIN");
 		}
 		Debug.Log("Enter");
 	}
 
-    private void OnCollisionStay(Collision collision)
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
+		if (other.gameObject.CompareTag("Player"))
+		{
 			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
 			Debug.Log("StayIN");
-        }
+		}
 		Debug.Log("Stay");
 	}
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-		if (collision.gameObject.CompareTag("Player"))
-        {
+		if (other.gameObject.CompareTag("Player"))
+		{
 			camTransform.localPosition = originalPos;
 			Debug.Log("ExitIN");
 		}
