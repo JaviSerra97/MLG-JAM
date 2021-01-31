2d5238a2c3aab77e29def62c6a787e9a6155da4d
jose manuel herrera vera <josem.hv@hotmail.es>
2021-01-31 11:47:34 +0100

    CameraShake

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.CompareTag("Player"))
+        {
+			originalPos = camTransform.localPosition;
+			Debug.Log("EnterIN");
+		}
+		Debug.Log("Enter");
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			Debug.Log("StayIN");
+        }
+		Debug.Log("Stay");
+	}
+
+    private void OnCollisionExit(Collision collision)
+    {
+		if (collision.gameObject.CompareTag("Player"))
+        {
+			camTransform.localPosition = originalPos;
+			Debug.Log("ExitIN");
+		}
+		Debug.Log("Exit");
+	}
+}
